5693e8e868652d66488914480c7d4060cd601b96
Huang Jifan <huangjifan1819@163.com>
2019-04-29 23:49:16 +0800

    Pay order function

--- web/controllers/api/Find.py
+++ web/controllers/api/Find.py
@@ -9,4 +9,13 @@ from flask import request, json, jsonify
 @route_api.route("/find/beacon", methods=['GET', 'POST'])
 def Beacon():
     resp = {"code": 200, "msg": "登录成功", "data": {}}
+    req = request.values
+    beacon_info = req['beacon_info'] if 'beacon_info' in req else ''
+    beacons = []
+    for i in beacon_info:
+        beacons.append(json.dumps(i))
+    for beacon in beacons:
+        beacon_uuid = BeaconInfo.query.filter_by(uuid=beacon.uuid).first()
+        if beacon_uuid:
+            pass
     return jsonify(resp)
